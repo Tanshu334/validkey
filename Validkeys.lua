f53fd4870b2de064d5f5db1f603adb53db73d086
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2022-11-07 16:07:02 +0530

    .lua

---
+++ Validkeys.lua
@@ -0,0 +1,19 @@
+return {
+
+["Ek8MZu8IAY7iX9o1BqlFJi6jRaiMVDr1"] = true,
+
+["lpFwPZNwLKDISu16O165gKUKscE29FAq"] = true,
+
+["a0QryPQjCcbubzA8sF7ZRkx1pboTlzfL"] = true,
+
+["ImMXSeSAzb5RRHC66W6d8sgYGeR6ruN4"] = true,
+
+["DHrNId8j2BSKwTpt7rNWiLa8DQRTe7Ya"] = true,
+
+["ZeY2fO5HbFmU6mY1XZioeHRHaAq6HUQU"] = true,
+
+["TYUNoRLRlD6sdzTi5doLhpBX8qZPPfDv"]= true,
+
+["RlCjnkja9qx99vjeDIk531krBcQRf32B"] = true
+
+}
